5ec231151d37ae0ed3f1ea1dbeb5a9cc1903980d
RikkiRuttProf <rikkirutt@gmail.com>
2026-05-28 18:11:08 -1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'unr-v2';
+const CACHE_NAME = 'unr-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
